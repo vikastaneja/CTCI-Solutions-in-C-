5b7f578823e58e81951b1c2f5501f053ee96f338
unknown <v-vtane@SI-110.redmond.corp.microsoft.com>
2014-11-26 01:25:37 -0800

    Chapter 1 - Question 1

---
+++ CTCI/CTCI-Tests/Chapter1Test.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CTCI;
+namespace CTCI.Tests
+{
+    [TestClass()]
+    public class Chapter1Test
+    {
+        private Chapter1 chapter1 = null;
+
+        [TestInitialize()]
+        public void TestInitalize()
+        {
+            if (this.chapter1 == null)
+            {
+                this.chapter1 = new Chapter1();
+            }
+        }
+
+        [TestCleanup()]
+        public void TestCleanup()
+        {
+            this.chapter1 = null;
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Q1_AllUniqueWithDictionaryTestNullInput()
+        {
+            this.chapter1.Q1_AllUniqueWithDictionary(null);
+        }
+
+        [TestMethod()]
+        public void Q1_AllUniqueWithDictionaryTestEmptyInput()
+        {
+            Assert.IsTrue(this.chapter1.Q1_AllUniqueWithDictionary(string.Empty), "Input string null is returning false");
+        }
+
+        [TestMethod()]
+        public void Q1_AllUniqueWithDictionaryTestAllUniqueInput()
+        {
+            Assert.IsTrue(this.chapter1.Q1_AllUniqueWithDictionary("string.Empy"), "Input string with all unique is returning false");
+        }
+
+        [TestMethod()]
+        public void Q1_AllUniqueWithDictionaryTestNonUniqueInput()
+        {
+            Assert.IsFalse(this.chapter1.Q1_AllUniqueWithDictionary("string.Empties"), "Input string with non-unique is returning true");
+        }
+    }
+}
---
+++ CTCI/CTCI/Chapter1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTCI
+{
+    public class Chapter1
+    {
+        public bool Q1_AllUniqueWithDictionary(string str)
+        {
+            if (str == null)
+            {
+                throw new ArgumentNullException("str");
+            }
+
+            if (str.Length == 0)
+            { return true; }
+
+            IDictionary<char, int> dict = new Dictionary<char, int>();
+            foreach (char c in str)
+            {
+                if (dict.ContainsKey(c))
+                    return false;
+
+                dict.Add(c, 1);
+            }
+
+            return true;
+        }
+    }
+}
