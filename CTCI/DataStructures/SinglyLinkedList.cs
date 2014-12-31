4fb9d2152767cb8780465d985e8673427553b0d0
vikastaneja <Par1M0na>
2014-12-31 00:28:10 -0800

    Addition of template for circular linked list question CTCI 2.6

---
+++ CTCI/DataStructures/SinglyLinkedList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class SinglyLinkedList
+    {
+        public static SingleLinkedListNode Head { get; private set; }
+        public static void AddToEndOfList(SingleLinkedListNode node)
+        {
+            if (node == null)
+            {
+                throw new ArgumentNullException("node");
+            }
+
+            if (Head == null)
+            {
+                Head = node;
+                return;
+            }
+
+            SingleLinkedListNode temp = Head;
+            while (temp.Next != null)
+            {
+                temp = temp.Next;
+            }
+
+            temp.Next = node;
+        }
+    }
+}
